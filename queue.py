2a7d0753ac2cb658fed16851111631d574b0fbbd
cmcdowell <chris.p.mcdowell@googlemail.com>
2013-01-22 04:14:02 +0000

    Added queue class

---
+++ queue.py
@@ -0,0 +1,46 @@
+class Queue(object):
+
+    def __init__(self, size_max):
+        assert size_max > 0
+        self.max = size_max
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+        self.data = range(size_max)
+
+    def __str__(self):
+        return str(self.data)
+
+    def __len__(self):
+        return self.size
+
+    def latest(self):
+        if self.size == 0:
+            return None
+        return self.data[self.head]
+
+    def empty(self):
+        return self.size == 0
+
+    def full(self):
+        return self.size == self.max
+
+    def enqueue(self, x):
+        if self.size == self.max:
+            return False
+        self.data[self.tail] = x
+        self.size += 1
+        self.tail += 1
+        if self.tail == self.max:
+            self.tail = 0
+        return True
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        x = self.data[self.head]
+        self.size -= 1
+        self.head += 1
+        if self.head == self.max:
+            self.head = 0
+        return x
