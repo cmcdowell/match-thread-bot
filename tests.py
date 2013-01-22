5ed24df530f02bbe8f606c163670191547efa634
cmcdowell <chris.p.mcdowell@googlemail.com>
2013-01-22 05:23:15 +0000

    Added unit tests for Match Class

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from queue import Queue
+from match import Match
+from datetime import datetime
 import unittest
 
 
@@ -14,6 +16,13 @@ class TestQueue(unittest.TestCase):
         self.assertEquals(len(self.q), 0)
         self.q.enqueue('test')
         self.assertEquals(len(self.q), 1)
+
+    def test_dequeue_empty_queue(self):
+        self.assertIsNone(self.q.dequeue())
+
+    def test_enqueue_full_queue(self):
+        self.q.enqueue('test')
+        self.assertFalse(self.q.enqueue('test'))
 
     def test_latest(self):
         self.assertEquals(self.q.latest(), None)
@@ -33,6 +42,39 @@ class TestQueue(unittest.TestCase):
     def test_dequeue(self):
         self.q.enqueue('test')
         self.assertEquals(self.q.dequeue(), 'test')
+
+
+class TestMatch(unittest.TestCase):
+
+    def setUp(self):
+        row = (2,
+               u'2013-01-20 16:00:00',
+               u'Tottenham Hotspur',
+               u'Manchester United',
+               u'White Hart Lane, London',
+               u'premier-league',
+               0)
+        self.m = Match(row)
+
+    def test_home_team(self):
+        self.assertIsInstance(self.m.home_team, unicode)
+        self.assertEquals(self.m.home_team, u'Tottenham Hotspur')
+
+    def test_away_team(self):
+        self.assertIsInstance(self.m.away_team, unicode)
+        self.assertEquals(self.m.away_team, u'Manchester United')
+
+    def test_venue(self):
+        self.assertIsInstance(self.m.venue, unicode)
+        self.assertEquals(self.m.venue, u'White Hart Lane, London')
+
+    def test_datetime(self):
+        test_date = datetime.strptime(u'2013-01-20 16:00:00', '%Y-%m-%d %H:%M:%S')
+        self.assertIsInstance(self.m.kick_off, datetime)
+        self.assertEquals(self.m.kick_off, test_date)
+
+    def test_played(self):
+        self.assertFalse(self.m.played)
 
 if __name__ == '__main__':
     unittest.main()
